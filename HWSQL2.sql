c8d0bec9aacf030f4abac3652a42f6008e4b7365
Rezeda1005 <alciona6@mail.ru>
2022-06-27 08:29:44 +0300

    adding files

---
+++ HWSQL2.sql
@@ -0,0 +1,44 @@
+create table if not exists Style(
+	id SERIAL primary key,
+	name VARCHAR(50) not null unique
+);
+
+create table if not exists Style_singer(
+	id_style INTEGER references Style(id),
+	id_singer INTEGER references Singer(id)
+);
+
+create table if not exists Singer(
+	id SERIAL primary key,
+	nickname VARCHAR(100) not null unique
+);
+
+create table if not exists Album(
+	id SERIAL primary key,
+	album_title VARCHAR(100) not null,
+	release_year INTEGER not null CHECK(year > 0)
+);
+
+create table if not exists Album_singer(
+	id_album INTEGER references Album(id),
+	id_singer INTEGER references Singer(id)
+);
+
+create table if not exists Track(
+	id SERIAL primary key,
+	track_name VARCHAR(100) not null,
+	duration NUMERIC not null CHECK(duration > 0)
+);
+
+create table if not exists Collection(
+  id serial primary key,  
+  name_collection varchar(50) not null,
+  release_year integer not null check(year> 0)
+);
+
+create table if not exists Track_collection(
+	id_collection INTEGER references Collection(id),
+	id_track INTEGER references Track(id)
+);
+
+
